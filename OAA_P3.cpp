f5b8f2f312ed6830938a01e5e12e7b180aaf80b2
OvsyankinaAA <a.a.ovsyankina@gmail.com>
2020-09-21 18:04:04 +0300

    main4

--- OAA_P3.cpp
+++ OAA_P3.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-<<<<<<< HEAD
     cout<<Hello world!<<endl;
     cout<<My New Rating<<endl;
-=======
     cout<<Comment was added<<endl;
->>>>>>> Comment
 }
